9e632d8bc00cde48fc6caf71d961648ae73d77d6
Chris Zhou <chris.zhou@activenetwork.com>
2017-08-03 14:41:11 +0800

    add svg icon

--- button.js
+++ button.js
@@ -1,23 +1,22 @@
 'use strict';
 
-var bodyDiv = document.getElementsByTagName("body"),
-  button = document.createElement("a"),
-  span = document.createElement("span"),
-  textnode = document.createTextNode("top");
+var bodyDiv = document.body,
+  button = document.createElement('a'),
+  svg = '<svg width="30" height="30" viewBox="0 0 1024 1024"><path d="M792.748 599.403l-257.312-259.001c-5.7-5.73-13.683-8.998-23.437-8.998s-16.541 3.881-21.63 8.971l-261.758 260.585c-11.205 11.174-11.231 29.313-0.031 40.513 13.731 13.731 34.927 5.614 40.544 0.056l214.226-213.168v372.648c0 15.844 12.835 28.653 28.649 28.653 15.817 0 28.653-12.813 28.653-28.653v-374.053l211.469 212.845c5.587 5.617 12.981 8.453 20.311 8.453 7.337 0 14.615-2.784 20.202-8.338 11.257-11.148 11.288-29.313 0.113-40.514v0 0zM827.161 251.635h-630.316c-15.817 0-28.653-12.835-28.653-28.645 0-15.818 12.835-28.653 28.653-28.653h630.316c15.84 0 28.645 12.835 28.645 28.653 0 15.81-12.805 28.645-28.645 28.645v0 0zM827.161 251.635z"></path></svg>';
 
-button.className = "btn btn-large btn-outline github-sst hidden";
+button.id = 'github-sst'
+button.innerHTML = svg
+bodyDiv.appendChild(button);
 
-button.appendChild(span);
-span.appendChild(textnode);
-bodyDiv[0].appendChild(button);
-
-button.addEventListener("click",runScroll,false);
+button.addEventListener('click',runScroll,false);
 
 window.onscroll = function() {
   if (document.body.scrollTop > 468) {
-    button.className = "btn btn-large btn-outline github-sst";
+    button.classList.remove('hidden');
+    button.classList.add('shown');
   } else {
-    button.className = "hidden";
+    button.classList.remove('shown');
+    button.classList.add('hidden');
   }
 
 }
